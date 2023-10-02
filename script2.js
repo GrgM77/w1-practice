7a01403971988550f73617882f942e0d2f1f1026
GrgM77 <gergomihaly23@gmail.com>
2023-10-02 14:19:19 +0300

    change

---
+++ script2.js
@@ -0,0 +1,19 @@
+//function numberek(number1, number2, number3, number4) {
+    console.log(number1, number2, number3, number4)
+    let logger =(number1 / number2 + number3 - number4)
+    return logger
+ //
+
+  function logger(number1, number2, number3, number4){
+    console.log(number1, number2, number3, number4)
+//let number1=2
+let number2=6
+let=number3=6
+let number4=78//
+let sumOfFiveNumbers=number1 * number2 * number3 * number4
+
+return sumOfFiveNumbers
+  }
+
+logger(number1, number2, number3, number4(6 * 7 * 6 * 6))
+
