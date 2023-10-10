263f2729bff15cf6fadb513d8db05bedebb0e355
GrgM77 <gergomihaly23@gmail.com>
2023-10-10 17:06:27 +0300

    practice1

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-let number1=4
-let number2=1000
-let number3=7
-let number4=8
+
+
 
 function logger(sgt) {
     console.log(sgt)
+
+logger()
 }
-logger(Math.floor)
-functionmultipleFunc(number1, number2, number3, number4) {
-    return number1 * number2 * number3 * number4
+functionmultipleFunc( number2, number3, number4), {
+    let result
 }
+    
+logger(math.floor(functionmultipleFunc))
+
+    
+
+
 logger(functionmultipleFunc(number1, number2, number3, number4))
 
 
+for (let i = 1; i <= 5; i++) {
+    console.log(i)
+  }
